63d7ddd11b19363c7926eb25b23c0e080a6ea11f
Ousmane Sow <ousmane.sow@neo9.fr>
2017-11-15 16:58:48 +0100

    security: Test password encryption

--- src/test/java/fr/april/gps/webapi/authentication/services/CryptoServiceTest.java
+++ src/test/java/fr/april/gps/webapi/authentication/services/CryptoServiceTest.java
@@ -1,8 +1,12 @@
 package fr.april.gps.webapi.authentication.services;
 
+import fr.april.gps.webapi.ApplicationTest;
+import fr.april.gps.webapi.common.configurations.security.services.ICryptoService;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -12,24 +16,21 @@ import org.springframework.test.context.web.WebAppConfiguration;
  * for GPS
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-//@SpringApplicationConfiguation
+@SpringBootTest(classes = ApplicationTest.class)
 @WebAppConfiguration
 @ActiveProfiles("test")
 public class CryptoServiceTest {
-	//
-//	@Autowired
-//	private ICryptoService cryptoService;
-//
-//	@Test
-//	public void testPasswordEncryption() {
-//		String password = "myPassword";
-//		String encryptedPassword = cryptoService.encryptPassword(password);
-//		Assert.assertNotEquals(password, encryptedPassword);
-//		Assert.assertTrue(cryptoService.checkPassword(password, encryptedPassword));
-//		Assert.assertFalse(cryptoService.checkPassword(password + "toto", encryptedPassword));
-//	}
+
+	@Autowired
+	private ICryptoService cryptoService;
+
 	@Test
-	public void testOK() {
-		Assert.assertTrue(true);
+	public void testPasswordEncryption() {
+		String password = "myPassword";
+		String encryptedPassword = cryptoService.encryptPassword(password);
+		Assert.assertNotEquals(password, encryptedPassword);
+		Assert.assertTrue(cryptoService.checkPassword(password, encryptedPassword));
+		Assert.assertFalse(cryptoService.checkPassword(password + "toto", encryptedPassword));
 	}
+
 }
